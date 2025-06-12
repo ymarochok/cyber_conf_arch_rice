844aaa42b99cb1f9c2afbf31988dcaf4fcf46807
Yaroslav Marochok <ymarochok@gmail.com>
2025-06-12 15:39:54 +0200

    Initial configuration files

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+    alias inthenameofemperor="sudo"
+    alias tlauncherCommand="java -jar programs/tlauncher/TLauncher.v14/TLauncher.jar"
+    alias gitbless="git init"
+    alias prayforcommit="git push"
+    alias servitors="htop"
+    alias codex="code-insiders ./"
+    alias bless="sudo pacman -S"
+    alias imeprialArchives="cd ~/projects/"
+    alias openrelic="nvim"
+    alias purge="sudo pacman -Rns"
+    alias purgeheretics="sudo pacman -Scc"
+end
